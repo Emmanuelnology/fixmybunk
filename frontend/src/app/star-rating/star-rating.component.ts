64bb4cb4368af9b7157c6bbaf4bc46dce496be23
Satyam Gehani <Satyam.Gehani@nology.io>
2019-01-16 13:04:26 +0000

    star rating kind of working

--- frontend/src/app/star-rating/star-rating.component.ts
+++ frontend/src/app/star-rating/star-rating.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarRatingComponent implements OnInit {
 
+  // private ratingVariable = 4;
+  // private noOfStars = 5;
+  // currentRate = 8;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  // ratingCalc(ratingVariable, noOfStars) {
+    
+  //   console.log(ratingVariable/noOfStars);
+  //   const rating = ((ratingVariable/noOfStars) * 10)
+  // }
+
+  starList: boolean[] = [true,true,true,true,true];
+  rating:number;  
+
+  setStar(data:any){
+        this.rating=data+1;                               
+        for(var i=0;i<=4;i++){  
+          if(i<=data){  
+            this.starList[i]=false;  
+          }  
+          else{  
+            this.starList[i]=true;  
+          }  
+       }  
+   }  
 
 }
