90fba9661702d65f1380097c0af6bd030f0a9be8
Georgina Ryder <Georgina.Ryder@nology.io>
2019-01-16 10:06:46 +0000

    profile-page folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { FunctionDemoComponent } from './function-demo/function-demo.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FunctionDemoComponent
+    FunctionDemoComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
