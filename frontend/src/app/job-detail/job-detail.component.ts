aebcf252e8661bffe63a7642f0ca429c3f268b28
Emmanuel Shutsha <Emmanuel.Shutsha@nology.io>
2019-01-17 17:03:31 +0000

    Quote function started

--- frontend/src/app/job-detail/job-detail.component.ts
+++ frontend/src/app/job-detail/job-detail.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IQuote {
+  text: string;
+  price: string;
+}
+
 @Component({
   selector: 'app-job-detail',
   templateUrl: './job-detail.component.html',
   styleUrls: ['./job-detail.component.scss']
 })
 export class JobDetailComponent implements OnInit {
+  public isQuoteAccepted = true;
   constructor() {
+  }
+
+  public accept () {
+    if (this.isQuoteAccepted) {
+      const quote: IQuote = {
+        text: 'The LandLord has Accepted Your Quote',
+        price: '£280',
+      };
+    } else {
+      const quote: IQuote = {
+      text: 'You Quoted',
+      price: '£280',
+    };
+  }
   }
   ngOnInit () {
   }
