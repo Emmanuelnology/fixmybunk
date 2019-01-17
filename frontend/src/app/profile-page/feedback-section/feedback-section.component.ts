7ba0597226f9f3003e985f16b73c8e150c898a68
Georgina Ryder <Georgina.Ryder@nology.io>
2019-01-17 11:59:26 +0000

    FEEDBACK BREAKPOINTS FIXED

--- frontend/src/app/profile-page/feedback-section/feedback-section.component.ts
+++ frontend/src/app/profile-page/feedback-section/feedback-section.component.ts
@@ -23,7 +23,7 @@ export class FeedbackSectionComponent implements OnInit {
       description: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.'
     },
     {
-      name: 'Keiran',
+      name: 'Kieran',
       description: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.'
     },
     {
