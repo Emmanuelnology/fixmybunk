29fcc4636391c2741cd710942fb196671250a4cf
Ben Tomaney <Ben.Tomaney@nology.io>
2019-01-23 16:17:27 +0000

    Breadcrumbs final link and home link fully working

--- frontend/src/app/breadcrumbs/breadcrumbs.component.ts
+++ frontend/src/app/breadcrumbs/breadcrumbs.component.ts
@@ -15,48 +15,18 @@ interface IBreadcrumb {
 export class BreadcrumbsComponent implements OnInit {
 
   breadcrumbs: IBreadcrumb[] = [];
-  path;
-  url;
-  title;
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    // this.path = this.activatedRoute.root;
-    this.url = this.activatedRoute.pathFromRoot[1].routeConfig.path;
-    // this.title = this.activatedRoute.pathFromRoot[0].snapshot.data.title;
     const path = this.activatedRoute.pathFromRoot;
     for (let i = 0; i  < path.length; i++) {
       const currentURL = path[i].routeConfig ? path[i].routeConfig.path : '';
-      // const currentUrl = path[i].snapshot.url;
       const loopTitle = path[i].routeConfig ? path[i].routeConfig.data.title : 'Home';
       if (!(i !== 0 && currentURL === '')) {
         this.breadcrumbs.push({label: loopTitle, url: currentURL});
       }
     }
     this.breadcrumbs[this.breadcrumbs.length - 1].last = true;
-      // const title = this.activatedRoute.routeConfig.data.title;
-      // const currentUrl = this.router.url;
-      // this.breadcrumbs.push({label: title, url: currentUrl, last: true});
   }
-
-  // ngOnInit() {
-  //   this.breadcrumbs = this.buildBreadcrumbs(this.activatedRoute.root, '', []);
-  // }
-
-  // buildBreadcrumbs(route: ActivatedRoute, url: string, breadcrumbs: IBreadcrumb[]) {
-  //   const label = route.routeConfig ? route.routeConfig.data.title : 'Home';
-  //   const path = route.routeConfig ? route.routeConfig.path : '';
-  //   const nextUrl = `${url}${path}`;
-  //   const breadcrumb = {label: label, url: nextUrl, last: false};
-  //   if (nextUrl === url) {
-  //     breadcrumb.last = true;
-  //   }
-  //   const newBreadcrumbs = [...breadcrumbs, breadcrumb];
-  //   if (nextUrl !== url) {
-  //     return this.buildBreadcrumbs(route, nextUrl, newBreadcrumbs);
-  //   }
-  //   return newBreadcrumbs;
-  // }
-
 }
