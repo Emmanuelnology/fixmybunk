d64a43851e474392ccfac9036288c3d992827ec4
Ben Tomaney <Ben.Tomaney@nology.io>
2019-01-23 14:58:41 +0000

    Breadcrumbs using path and title of component and home page

--- frontend/src/app/breadcrumbs/breadcrumbs.component.ts
+++ frontend/src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
 
 interface IBreadcrumb {
   label: string;
@@ -14,32 +14,47 @@ interface IBreadcrumb {
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  breadcrumbs: IBreadcrumb[] = [
-    {label: 'Home', url: '/'},
-    {label: 'Another Page', url: '/'}
-  ];
+  breadcrumbs: IBreadcrumb[] = [];
   path;
   url;
   title;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    // this.path = this.activatedRoute.pathFromRoot;
+    // this.path = this.activatedRoute.root;
     // this.url = this.activatedRoute.pathFromRoot[1].snapshot.url;
-    // // this.title = this.activatedRoute.pathFromRoot[1].snapshot.data.title;
-    // const path = this.activatedRoute.pathFromRoot;
-    // for (let i = 0; i  < path.length; i++) {
-    //   const currentUrl = '/';
-    //   // const currentUrl = path[i].snapshot.url;
-    //   const title = 'Hi';
-    // //   const title = this.path[i].routeConfig.data.title;
-    //   this.breadcrumbs.push({label: title, url: currentUrl});
-    // }
-    // this.breadcrumbs[path.length - 1].last = true;
-      const title = this.activatedRoute.routeConfig.data.title;
-      const currentUrl = this.activatedRoute.routeConfig.path;
-      this.breadcrumbs.push({label: title, url: currentUrl, last: true});
+    // this.title = this.activatedRoute.pathFromRoot[0].snapshot.data.title;
+    const path = this.activatedRoute.pathFromRoot;
+    for (let i = 0; i  < path.length; i++) {
+      const currentURL = path[i].routeConfig ? path[i].routeConfig.path : '';
+      // const currentUrl = path[i].snapshot.url;
+      const loopTitle = path[i].routeConfig ? path[i].routeConfig.data.title : 'Home';
+      this.breadcrumbs.push({label: loopTitle, url: currentURL});
+    }
+    this.breadcrumbs[path.length - 1].last = true;
+      // const title = this.activatedRoute.routeConfig.data.title;
+      // const currentUrl = this.router.url;
+      // this.breadcrumbs.push({label: title, url: currentUrl, last: true});
   }
+
+  // ngOnInit() {
+  //   this.breadcrumbs = this.buildBreadcrumbs(this.activatedRoute.root, '', []);
+  // }
+
+  // buildBreadcrumbs(route: ActivatedRoute, url: string, breadcrumbs: IBreadcrumb[]) {
+  //   const label = route.routeConfig ? route.routeConfig.data.title : 'Home';
+  //   const path = route.routeConfig ? route.routeConfig.path : '';
+  //   const nextUrl = `${url}${path}`;
+  //   const breadcrumb = {label: label, url: nextUrl, last: false};
+  //   if (nextUrl === url) {
+  //     breadcrumb.last = true;
+  //   }
+  //   const newBreadcrumbs = [...breadcrumbs, breadcrumb];
+  //   if (nextUrl !== url) {
+  //     return this.buildBreadcrumbs(route, nextUrl, newBreadcrumbs);
+  //   }
+  //   return newBreadcrumbs;
+  // }
 
 }
